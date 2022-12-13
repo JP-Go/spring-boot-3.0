09fffa9207ab2d3fd6bfd44da08122e848fa636c
João Pedro <jpedro1580@gmail.com>
2022-12-13 00:26:23 -0300

    Setup put and delete mappings

--- src/main/java/com/jp/Main.java
+++ src/main/java/com/jp/Main.java
@@ -3,7 +3,12 @@ package com.jp;
 import java.util.List;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +30,37 @@ public class Main {
   @GetMapping
   public List<Customer> getCustomer() {
     return customerRepository.findAll();
+  }
+
+  record NewCustomerRequest(String name, String email, Integer age) {}
+
+  @PostMapping
+  public void addCustomer(@RequestBody NewCustomerRequest request) {
+    Customer customer = new Customer();
+    customer.setName(request.name());
+    customer.setEmail(request.email());
+    customer.setAge(request.age());
+
+    customerRepository.save(customer);
+  }
+
+  @DeleteMapping("{customerId}")
+  public void removeCustomer(@PathVariable("customerId") Integer customerId) {
+    customerRepository.deleteById(customerId);
+  }
+
+  record UpdateCustomerRequest(String name, String email, Integer age) {}
+
+  @PutMapping("{customerId}")
+  public void updateCustomer(@PathVariable("customerId") Integer customerId,
+                             @RequestBody UpdateCustomerRequest request) {
+    Customer customer = customerRepository.findById(customerId)
+                            .orElseThrow(RuntimeException::new);
+    if (request.name() != null && request.name().length() > 0)
+      customer.setName(request.name());
+    if (request.email() != null && request.email().length() > 0)
+      customer.setEmail(request.email());
+    if (request.age() != null && request.age() > 0)
+      customer.setAge(request.age());
   }
 }
